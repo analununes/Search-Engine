13d66136fd2a75f18f2eed6b49a81926fe27ed29
Ana <53786425+analununes@users.noreply.github.com>
2020-06-03 14:14:52 -0300

    Update suffix-tree.cpp

--- suffix-tree.cpp
+++ suffix-tree.cpp
@@ -6,13 +6,30 @@
 #include <bits/stdc++.h> 
 #include <cctype>
 #include <stdio.h>
+#include <time.h>
 
 using namespace std;
 
+struct func{
+    string textId;
+    string title;
+    int nTitle;
+    int nText;
+    func *alfaNext;
+    func *rankNext;
+
+    func(string n): textId(n){
+        nTitle = 0;
+        nText = 0;
+        alfaNext = nullptr;
+        rankNext = nullptr;
+        title = "\0";
+    }
+};
 
 struct Node {
     string suffix;
-    Node *adress;
+    func *address;
     Node *a;Node *b;Node *c;Node *d;Node *e;Node *f;Node *g;
     Node *h;Node *i;Node *j;Node *k;Node *l;Node *m;Node *n;
     Node *o;Node *p;Node *q;Node *r;Node *s;Node *t;Node *u;
@@ -21,7 +38,7 @@ struct Node {
     Node *n6;Node *n7;Node *n8;Node *n9;Node *n0;
 
     Node(string word): suffix(word){
-        adress = nullptr;
+        func *addres = nullptr;
         Node *a = nullptr; Node *b = nullptr; Node *c = nullptr; Node *d = nullptr; Node *e = nullptr;
         Node *f = nullptr; Node *g = nullptr; Node *h = nullptr; Node *i = nullptr; Node *j = nullptr;
         Node *k = nullptr; Node *l = nullptr; Node *m = nullptr; Node *n = nullptr; Node *o = nullptr; 
@@ -167,6 +184,7 @@ class SuffixTree{
         SuffixTree(): pRoot(nullptr){
         };
 
+
         void insert(string suffix) {
             Node **p;
             string curw;
@@ -175,8 +193,9 @@ class SuffixTree{
                     *p = new Node(suffix);
                 }else{
                     *p = new Node(suffix.substr(curw.length()));
+                
                 }
-                cout<<"sufixo "<<(*p)->suffix<<" inserida na árvore"<<endl;
+                cout<<"sufixo "<<(*p)->suffix<<" inserido na árvore"<<endl;
             }
         }
         
@@ -197,7 +216,6 @@ class SuffixTree{
                         *p = new Node(k.substr(0,i));
                         curw+=k.substr(0,i);
                         Node **t = p;
-                        cout<<curw<<endl;
                         s = k[i];
                         (table.find(s)->second)(p);
                         *p = new Node(k.substr(i));
@@ -213,7 +231,9 @@ class SuffixTree{
                     }
                 }
                 curw+=((*p)->suffix);
-                if(curw==suf)return true;
+                if(curw==suf){
+                    return true;
+                }
                 daddy = p;
                 curl = suf[curw.length()];
                 (table.find(curl)->second)(p);
@@ -228,53 +248,281 @@ class SuffixTree{
             string curl;
             while(*p){
                 curw+=((*p)->suffix);
-                cout<<curw<<endl;
                 if(curw==suf)return true;
                 curl = suf[curw.length()];
-                cout<<curl<<endl;
                 (table.find(curl)->second)(p);
             }
             return false;
         }
+        
+        int search(string suf, func **&k) {
+            Node **p;
+            p = &pRoot;
+            string curw;
+            string curl;
+            int resu;
+            int li = 0;
+            while(*p){
+                curw+=((*p)->suffix);
+                if(curw==suf){
+                    k = &((*p)->address);
+                    return 1;
+                    }
+                curl = suf[curw.length()];
+                (table.find(curl)->second)(p);
+            }
+            return 0;
+        }
 
+        bool insert_loc(string suf, string text_id, int read_t, string titulo) {
+            Node **p;
+            p = &pRoot;
+            string curw;
+            string curl;
+            func **k;
+            func **froot;
+            func **prev;
+            func **fnew;
+            func **aux;
+            int curn;
+            bool alf,rank = 0;
+            while(*p){
+                curw+=((*p)->suffix);
+                if(curw==suf){
+                    froot = &((*p)->address);
+                    //root da linked list com o numero de resultados
+                    if(*froot==nullptr){
+                        *froot = new func("*"); 
+                    }
+                    
+                    prev = froot;
+                    k = &((*froot)->rankNext);
+                    while(*k){
+                        if((*k)->textId == text_id){
+                            *fnew  = new func(text_id); 
+                            (*fnew)->title = titulo; 
+                            aux = &((*k)->alfaNext);
+                            (*fnew)->alfaNext = *aux;
+                            //if (read_t){
+                                curn = ((*k)->nTitle);
+                                curn += 10*read_t;
+                                (*fnew)->nTitle = curn;
+                            //}else{
+                                curn = ((*k)->nText);
+                                curn += !read_t;
+                                ((*fnew)->nText) = curn;
+                            //}
+                            k = &((*k)->rankNext);
+                            ((*prev)->rankNext) = *k;
+                            rankeada(froot,fnew);
+                            return true;
+                        }
+                        prev = k;
+                        k = &((*k)->rankNext);  
+                    }  
+                    
+                    curn = (*froot)->nText;
+                    curn +=  1;
+                    (*froot)->nText = curn;  
+                    *fnew = new func(text_id);
+                    (*fnew)->nTitle = read_t * 10;
+                    (*fnew)->nText  = !read_t;
+                    (*fnew)->title  = titulo;
+                    prev = froot;
+                    alfabetica(froot,fnew);
+                    rankeada(prev,fnew);
+                    return true;
+                }
+                curl = suf[curw.length()];
+                (table.find(curl)->second)(p);
+            
+            }
+            
+            return false;
+        }
+
+        bool alfabetica(func **&froot, func **&k){
+            string titulo = (*k)->title;
+            string curr;
+            func **prev;
+            int title,text;
+            prev = froot;
+            froot = &((*froot)->alfaNext);
+            while(*froot){
+                curr = (*froot)->title;
+                int i = 0;
+                int minimo = min(curr.length(),titulo.length());
+                for(i=0;i<minimo;i++){
+                    if (curr[i]>titulo[i]){
+                        froot = &((*prev)->alfaNext);
+                        (*k)->alfaNext = *froot;
+                        (*prev)->alfaNext = *k;
+                    return true;
+                    }else if(curr[i]<titulo[i]){
+                        i = minimo;
+                    }else{
+                        i ++;
+                        if (i==minimo && curr.length()>titulo.length()){
+                            cout<<"hm"<<endl;
+                            froot = &((*prev)->alfaNext);
+                            (*k)->alfaNext = *froot;
+                            (*prev)->alfaNext = *k;
+                        }
+                        
+                    }
+                }
+                
+                prev = froot;
+                froot = &((*froot)->alfaNext);
+            }
+            
+            (*prev)->alfaNext = *k;
+            return true;
+            //}
+        }
+
+        bool rankeada(func **&froot, func **&k){
+            int total = (*k)->nText + (*k)->nTitle;
+            int curr;
+            func **prev;
+            prev = froot;
+            while((*froot)){
+                curr = (*froot)->nText + (*froot)->nTitle;
+                if(curr<total){
+                    froot = &((*prev)->rankNext);
+                    (*k)->rankNext = *froot;
+                    (*prev)->rankNext = *k;
+                    return true;
+                }
+                prev = froot;
+                froot = &((*froot)->rankNext); 
+            }
+            (*prev)->rankNext = *k;
+            return true;
+        }
 };
+
+
 
 int main(){
     SuffixTree arvore;
     arvore.insert("*");
 
-    ofstream outfile ("test.txt");
-
-    //outfile << "O território peruano abrigou a civilização de Caral, uma das mais antigas do mundo, bem como o Império Inca, considerado o maior Estado da América pré-colombiana. O seu território foi elevado a vice-reinado pelo Império Espanhol, no século XVI. Atualmente, o Peru é uma república presidencialista democrática dividida em 25 regiões. A sua geografia é variada, exibindo desde planícies áridas na costa do Pacífico, aos picos nevados dos Andes e à floresta amazônica, características que proporcionam a este país diversos recursos naturais."<< endl;
-    outfile << "Peruvian territory was home to several ancient cultures. Ranging from the Norte Chico civilization starting in 3500 BC, the oldest civilization in the Americas and one of the five cradles of civilization, to the Inca Empire, the largest state in pre-Columbian America, the territory now including Peru has one of the longest histories of civilization of any country, tracing its heritage back to the 4th millennia BCE." <<endl;
-    outfile.close();
-
     // filestream variable file 
     fstream file; 
-    string word, t, q, filename, a; 
-    int i;
+    string word, t, q, filename, a, titulo, text_id, suf; 
+    int i,j, read_t;
+    bool notText,reading;
+
     // filename of the file 
-    filename = "test.txt"; 
-  
+    filename = "englishText_20000_30000"; 
+    
     // opening file 
     file.open(filename.c_str()); 
-  
+    if(!file){
+        cout<<"erro"<<endl;
+    }
     // extracting words from the file 
+    reading = 1;
+
     while (file >> word) { 
         i = 0;
         a+='*';
-        while (word[i] != '\0') {
-        if ((word[i] > 47 && word[i] < 58) || (word[i] > 64 && word[i] < 91) || (word[i] > 96 && word[i] < 123) ){
-            a += tolower(word[i]);
+        cout<<word<<endl;
+        if(word[0]==60){
+            notText = 1;
         }
-        i++;
+
+        if(notText){
+            cout<<"nottext"<<endl;
+            if(word.substr(0,3)=="id="){
+                text_id = word;
+            }
+
+            if (read_t == 1){
+                if(word.substr(0,12)==("nonfiltered=")){
+                    suf = "\0";
+                    for(j = 0;j<titulo.length();j++){
+                        suf +=titulo[j];
+                        if(titulo[j]==32 || j==titulo.length()-1){
+                            suf += "\0";
+                            i = 0;
+                            a = "*";
+                        while (suf[i] != '\0') {
+                            if ((suf[i] > 47 && suf[i] < 58) || (suf[i] > 64 && suf[i] < 91) || (suf[i] > 96 && suf[i] < 123) || (suf[i]!=32) ){
+                                a += tolower(suf[i]);
+                            }
+                            i++;
+                        }
+                        arvore.insert(a);
+                        arvore.insert_loc(a,text_id,read_t,titulo);
+                        cout<<a<<endl;
+                        cout<<read_t<<endl;
+                        a.clear();
+                        suf.clear();
+                    }
+                    }
+                    read_t = 0;
+                }else{
+                    titulo += " " + word;
+                }
+            }else if(word.substr(0,6)==("title=")){
+                read_t = 1;
+                word = word.substr(6);
+                titulo = word;
+            }
+
+        }else{
+            cout<<"outra coisa"<<endl;
+                while (word[i] != '\0') {
+                    if ((word[i] > 47 && word[i] < 58) || (word[i] > 64 && word[i] < 91) || (word[i] > 96 && word[i] < 123) ){
+                        a += tolower(word[i]);
+                    }
+                    i++;
+                }
+                arvore.insert(a);
+                arvore.insert_loc(a,text_id,read_t,titulo);
         }
-        if (a!=" "){
-            arvore.insert(a);
-        }
-        // displaying content 
-        cout << a << endl; 
         a.clear();
+
+        if(word[word.length()-1]==62){
+            notText=0;
         }
+        
+    }
+
+    string query;
+    func **k;
+    cout<<"digite sua pesquisa:";
+    cin>>query;
+    clock_t Ticks[2];
+    Ticks[0] = clock();
+
+    if (arvore.search("*"+query,k)){
+        Ticks[1] = clock();
+        double Tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+        cout<<(*k)->nText<<" resultados encontrados em "<< Tempo<< "ms"<<endl;
+        cout<<(*k)->alfaNext<<endl;
+        *k = (*k)->alfaNext;
+        i = 0;
+        while(*k){
+            cout<<i<<"->"<<(*k)->title<<endl;
+            cout<<(*k)->nText<<endl;
+            cout<<(*k)->nTitle<<endl;
+            cout<<(*k)->alfaNext<<endl;
+            i +=1;
+            if (i%20==0){
+                cout<<"ver mais resultados? [s/n]"<<endl;
+                string ask;
+                cin>>ask;
+                if (ask == "n"){
+                    break;
+                }
+            }
+            *k = (*k)->alfaNext;
+        }
+    }else{
+        cout<<"não encontrado"<<endl;
+    }
     return 0;
 };
